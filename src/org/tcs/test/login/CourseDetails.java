329ac5a7c0e16e28cf16526b2b836e7959295cbf
tamilsiva95 <tamilsiva2395@gmail.com>
2022-05-04 12:19:47 +0530

    adding new project

---
+++ src/org/tcs/test/login/CourseDetails.java
@@ -0,0 +1,26 @@
+package org.tcs.test.login;
+
+public class CourseDetails {
+
+	public static void main(String[] args) {
+		//DataType variableName = value;
+		
+		float empSalary = 987.9879f;
+		System.out.println("Employee salary is "+empSalary);
+		
+		float x = 20/3f;
+		System.out.println(x);
+		
+		double empAnswer = 987.98765587645;
+		System.out.println("Employee answer is "+empAnswer);
+		
+		double y = 20/3d;
+		System.out.println(y);
+		
+	}
+
+
+
+
+
+}
